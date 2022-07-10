a1099b3d11d3fc05c0f84898d1c21d94c6ae007f
galthran80 <rafal.jarosinski@gmail.com>
2022-07-10 16:39:44 +0200

    Update First ADC program

--- first_adc/first_adc.ino
+++ first_adc/first_adc.ino
@@ -3,6 +3,7 @@ float napiecie = 0;
 
 void setup() {
   Serial.begin(9600);
+  pinMode(7, OUTPUT);
 
 }
 
@@ -10,5 +11,12 @@ void loop() {
   odczytanaWartosc = analogRead(A5);
   napiecie = odczytanaWartosc * (5.0/1024.0);
   Serial.println(napiecie);
+
+  if (napiecie > 2.5) {
+    digitalWrite(7, HIGH);
+  } else {
+    digitalWrite(7, LOW);
+  }
+  
   delay(200);
 }
