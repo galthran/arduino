7e07df74bc0c5c356a432537b23d1987e9a81344
galthran80 <rafal.jarosinski@gmail.com>
2023-08-19 20:22:20 +0200

    Simple Voltmeter

---
+++ simple_voltmeter/simple_voltmeter.ino
@@ -0,0 +1,33 @@
+#include <LiquidCrystal.h> 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int odczytanaWartosc = 0;
+float napiecie = 0;
+
+void setup() {
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+  pinMode(7, OUTPUT);
+
+}
+
+void loop() {
+  odczytanaWartosc = analogRead(A5);
+  napiecie = odczytanaWartosc * (5.0/1024.0);
+  Serial.println(napiecie);
+
+  if (napiecie > 2.5) {
+    digitalWrite(7, HIGH);
+  } else {
+    digitalWrite(7, LOW);
+  }
+
+  lcd.setCursor(0,0); 
+  lcd.clear();
+  lcd.print("Voltmeter:");
+
+  lcd.setCursor(0,1);
+  lcd.print(napiecie);
+  lcd.print(" V");
+  delay(200);
+}
