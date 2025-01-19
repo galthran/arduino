60a900f37b09e1da83a555feb3ea8a91b8c6026f
Galthran <rafal.jarosinski@gmail.com>
2025-01-19 17:50:18 +0100

    Car LED blinker

---
+++ car_led_blinker/car_led_blinker.ino
@@ -0,0 +1,60 @@
+int i = 2;
+
+void setup() {
+  Serial.begin(9600);
+  
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  for (int j = 2; j <= i; j ++) {
+    digitalWrite(j, HIGH); 
+  }
+
+  delay(40);
+
+  i = i + 1;
+
+  if (i > 12) {
+    i = 1;
+
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(7, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(12, LOW);
+
+    delay(100);
+  }
+}
